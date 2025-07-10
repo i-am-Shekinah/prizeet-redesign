c7037e8ff607f4e25d9455ca25a8f7b5f3efda6a
Michael <MichaelOlatunji.dev@gmail.com>
2025-07-10 16:52:44 +0100

    Fix: alias path and node types for Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import path from 'path';
+import {
+  dirname,
+  resolve,
+} from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": resolve(__dirname, "src"),
     },
   },
-  plugins: [react()],
-})
+});
